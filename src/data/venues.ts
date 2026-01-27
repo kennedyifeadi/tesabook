0f44b1b82d672244506fc7eaa0bba7abae84dbf5
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2026-01-27 14:37:09 +0100

    feat: Initialize Next.js project with global layout, styling, Firebase integration, and venue data structures.

---
+++ src/data/venues.ts
@@ -0,0 +1,14 @@
+import { Venue } from "@/types/venue";
+
+export const seedVenues: Venue[] = [
+    { name: 'NLNG Front', capacity: 30 },
+    { name: 'NLNG Back', capacity: 10 },
+    { name: 'NFLT Front', capacity: 20 },
+    { name: 'NFLT Back', capacity: 10 },
+    { name: 'Inside Pet', capacity: 5 },
+    { name: 'Agric Back', capacity: 10 },
+    { name: 'Tractor Shed', capacity: 10 },
+    { name: 'Elect Tarmac', capacity: 20 },
+    { name: 'CBT', capacity: 25 },
+    { name: 'New Civil', capacity: 10 },
+];
